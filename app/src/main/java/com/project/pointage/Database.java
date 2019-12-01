cfa56c53e6fd1df90d44f9910fc09b5c175744bd
Clemant <capou.mj@gmail.com>
2019-12-01 20:26:38 +0100

    Make some modification on the Database

--- app/src/main/java/com/project/pointage/Database.java
+++ app/src/main/java/com/project/pointage/Database.java
@@ -1,4 +1,30 @@
 package com.project.pointage;
 
-public class Database {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final String DATABASE = "employe.db";
+    private static final String TABLE    = "employe";
+    private static final int VERSION     = 1;
+
+
+    public Database(Context context){
+        super(context, DATABASE, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String request ="CREATE TABLE "+this.TABLE+" (id INTEGER PRIMARY KEY, username TEXT, password TEXT, name TEXT,firstname TEXT, type INTEGER)";
+        sqLiteDatabase.execSQL(request);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String request = "DROP TABLE IF EXISTS "+this.DATABASE;
+        sqLiteDatabase.execSQL(request);
+        onCreate(sqLiteDatabase);
+    }
 }
