e8dc732175f30e039e5526f6342f1541572885bd
Clemant <capou.mj@gmail.com>
2019-12-01 20:29:32 +0100

    Add a regex for more security

--- app/src/main/java/com/project/pointage/ui/login/LoginActivity.java
+++ app/src/main/java/com/project/pointage/ui/login/LoginActivity.java
@@ -5,6 +5,10 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -13,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -23,12 +28,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.project.pointage.R;
-import com.project.pointage.ui.login.LoginViewModel;
+import com.project.pointage.*;
 import com.project.pointage.ui.login.LoginViewModelFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
+    private Database database = new Database(LoginActivity.this);
+    private Authentification authentification = new Authentification(LoginActivity.this);
+    private String username = null;
+    private String password = null;
+    private boolean isCheckUser = false;
+    private Message messenger = new Message();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,8 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
@@ -55,12 +68,15 @@ public class LoginActivity extends AppCompatActivity {
                 if (loginFormState.getPasswordError() != null) {
                     passwordEditText.setError(getString(loginFormState.getPasswordError()));
                 }
+
             }
+
         });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+             loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
             @Override
             public void onChanged(@Nullable LoginResult loginResult) {
+
                 if (loginResult == null) {
                     return;
                 }
@@ -74,7 +90,7 @@ public class LoginActivity extends AppCompatActivity {
                 setResult(Activity.RESULT_OK);
 
                 //Complete and destroy login activity once successful
-                finish();
+                //finish();
             }
         });
 
@@ -113,19 +129,57 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                username = usernameEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+
+                Log.i("debug","The username: "+username+" and the password: "+password);
+              /*  SQLiteDatabase sqLiteDatabase = database.getWritableDatabase();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("username",username);
+                contentValues.put("password",authentification.get_hash_pasword(password));
+                contentValues.put("name","CAPOU");
+                contentValues.put("firstname","Marie-Joseph");
+                contentValues.put("type",1);
+                sqLiteDatabase.insert("employe",null,contentValues);*/
+               isCheckUser = authentification.checkUser(username,password,database);
+               Log.i("debug","is User valid LogActivity: "+isCheckUser);
+               if(isCheckUser){
+                    loginViewModel.login(username,password);
+                }
+               else{
+                   loadingProgressBar.setVisibility(View.INVISIBLE);
+                   Log.i("debug","Login failed");
+
+                   messenger.message(LoginActivity.this,"Login failed","Le mot de passe et/ou l'identifiant est incorrect",0);
+               }
             }
         });
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
+
+
+        //SharedPReferences
+
+       // String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+       // Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,Employer.class);
+        startActivity(intent);
     }
+
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+        messenger.message(LoginActivity.this,"Error",""+errorString,0);
     }
+
+    @Override
+    public void onBackPressed() {
+
+        Toast.makeText(LoginActivity.this,"Hello",Toast.LENGTH_LONG).show();
+        Log.i("debug","Quiter ");
+        //super.onBackPressed();
+    }
+
 }
